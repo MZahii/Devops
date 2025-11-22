911d13b0d23984d34986b009a0b34be79d00e0ac
Mohamed Zahi <125976117+MZahii@users.noreply.github.com>
2025-11-22 17:13:47 +0100

    Update StudentManagementApplicationTests.java

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,8 +2,8 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import org.springframework.boot.test.context.TestComponent;
+@TestComponent
 class StudentManagementApplicationTests {
 
     @Test
