25c9f7c8d247870903588174049b1d27ea99944e
MohamedZahi <zehi.mohamed@yahoo.fr>
2025-12-14 19:50:56 +0100

    fix

--- frontend/src/app/services/department.service.ts
+++ frontend/src/app/services/department.service.ts
@@ -6,7 +6,7 @@ import { Department } from '../models/department.model';
 @Injectable({ providedIn: 'root' })
 export class DepartmentService {
   // Using Kubernetes NodePort 30080 for backend access
-  private readonly apiUrl = 'http://192.168.56.20:30080/student/department';
+  private readonly apiUrl = 'http://192.168.56.20:9999/student/department';
 
   constructor(private http: HttpClient) {}
 
